8ac654d7ffe7427ca0a3fbf71b11834de46a6911
193006 <137424083+193006@users.noreply.github.com>
2023-08-04 22:51:30 +0000

    minor word

--- app.py
+++ app.py
@@ -461,3 +461,13 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
+
+padding = 0
+st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True)
+
