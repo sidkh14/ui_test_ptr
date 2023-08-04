a4753f33af868aefcc021300bdb8e06d79eea6ec
193006 <137424083+193006@users.noreply.github.com>
2023-08-04 22:52:13 +0000

    minor word

--- app.py
+++ app.py
@@ -471,3 +471,10 @@ st.markdown(f""" <style>
         padding-bottom: {padding}rem;
     }} </style> """, unsafe_allow_html=True)
 
+[theme]
+primaryColor="#2214c7"
+backgroundColor="#ffffff"
+secondaryBackgroundColor="#e8eef9"
+textColor="#000000"
+font="sans serif"
+
