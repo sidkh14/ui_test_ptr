b018f29dbfb409afe6cd1d62099a2dd19fbe8c92
193006 <137424083+193006@users.noreply.github.com>
2023-08-05 18:33:35 +0000

    add the sidebar

--- app.py
+++ app.py
@@ -118,16 +118,14 @@ st.markdown("""
 st.title("SAR Evidence Processor")
 # st.subheader('Evidence Processor')
 with st.sidebar:
-    "## This is the sidebar"
-    st.sidebar.write("This is :blue[test]")
+    # st.sidebar.write("This is :blue[test]")
     # Adding Sidebar
     st.sidebar.image('logo.png', width=99)
     # Navbar
-
     st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
     st.markdown("""
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #005050;">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
