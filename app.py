b799fb8ac9b13e0625c9ec883a8e47c2dd002748
193006 <137424083+193006@users.noreply.github.com>
2023-08-07 17:14:10 +0000

    adding functionality

--- app.py
+++ app.py
@@ -160,6 +160,9 @@ with st.sidebar:
     </nav>
     """, unsafe_allow_html=True)
 
+    # Add the image to the sidebar below options
+    st.sidebar.image("MicrosoftTeams-image (7).png", use_column_width=True)
+
     # Add the app name
     st.sidebar.markdown('<p class="big-font">SARA</p>', unsafe_allow_html=True)
     # st.sidebar.header("SARA")
