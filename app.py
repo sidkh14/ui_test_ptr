f110ceb1cdd21690c678a347d015d65226c6e617
193006 <137424083+193006@users.noreply.github.com>
2023-08-09 11:32:22 +0000

    adding the logo

--- app.py
+++ app.py
@@ -599,8 +599,7 @@ with st.spinner('Summarization ...'):
         # showing the text in a textbox
         usr_review = st.text_area("", value=st.session_state["tmp_summary"])
         if st.button("Update Summary"):
-            st.session_state.tmp_summary = process_text(usr_review)
-        process_text(st.session_state.tmp_summary)
+            st.session_state["tmp_summary"] = process_text(usr_review)
         # st.write(st.session_state["tmp_summary"])
 
 
