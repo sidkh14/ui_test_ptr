0d3b35413900ae1ccdfcdab325f4afabe1fccbdd
193006 <137424083+193006@users.noreply.github.com>
2023-08-09 10:35:29 +0000

    adding the logo

--- app.py
+++ app.py
@@ -167,7 +167,8 @@ with st.sidebar:
             <a class="nav-link disabled" href="#">
                 <img src="https://www.exlservice.com/themes/exl_service/exl_logo_rgb_orange_pos_94.png" width="50" height="30" alt="">
                 <span class="sr-only">(current)</span>
-                <span style="color: #F4C430; font-weight: bold;">Operations Process Automation</span>
+                <p>Operations Process Automation</p>
+                <span style="color: #FDA27E; font-weight: bold;">Your Text Here</span>
             </a>
         </li>
         </ul>
