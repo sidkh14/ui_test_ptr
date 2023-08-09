188b92e4d76089832a358e815a7ec2ab56dabf2b
193006 <137424083+193006@users.noreply.github.com>
2023-08-09 10:04:30 +0000

    adding the logo

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ with st.sidebar:
         <ul class="navbar-nav">
         <li class="nav-item active">
             <a class="nav-link disabled" href="#">
-                <img src="	https://github.com/193006/doc_extractor/blob/main/exl_sar_logo.png" width="50" height="30" alt="">
+                <img src="https://github.com/193006/doc_extractor/blob/main/exl_sar_logo.png" width="50" height="30" alt="">
                 <span class="sr-only">(current)</span>
             </a>
         </li>
