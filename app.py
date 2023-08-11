49bb71021607ecb6dc7c7a7029d34782a68c2f61
Siddhant Khandelwal <44205451+sidkh14@users.noreply.github.com>
2023-08-11 14:40:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -280,6 +280,7 @@ memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=500)
 conversation = ConversationChain(llm=llm, memory =memory,verbose=False)
 
 def call_hf_flan_t5_xxl_api(prompt, **params):
+    prompt=prompt.split()[:1024]
     max_new_tokens = params.get('max_new_tokens', 200)
     temperature = params.get('temperature', None)
     top_k = params.get('top_k', None)
